e963b62172f4d07e8ac4a94c89d6f588f738d7db
Jonathan <jonno.5000@gmail.com>
2021-11-25 06:30:01 -0500

    Throw error on re-using a slot

--- src/svgame/entities/base/EntityBase.cpp
+++ src/svgame/entities/base/EntityBase.cpp
@@ -4,6 +4,10 @@
 
 // Constructor(s)
 EntityBase::EntityBase(ServerEntity* serverEntityHandle) {
+    if (serverEntityHandle->inUse) {
+        gi.Error("Attempted to allocate an entity into a non-free slot");
+    }
+
     this->entityHandle = serverEntityHandle;
     this->entityHandle->inUse = true;
 }
